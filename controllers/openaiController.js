ad33347e376809d117e1ab09408d2044b6b1499f
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-27 10:16:33 +0100

    ajouter la fonction qui permet de recuperer l url de l image et le passe les dimmentions souhaitées

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -5,11 +5,23 @@ apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-const generateImage = (req,res)=>{
-    res.status(200).json({
-        succes:true,
-        
-    })
+const generateImage =async (req,res)=>{
+    try {
+        const response = await openai.createImage({
+            prompt: 'Polar bear on ice skates',
+            n: 1,
+            size:'512x512'
+        })
+        const imageUrl = response.data.data[0].url;
+        res.status(200).json({
+            succes:true,
+            data:imageUrl
+        })
+    } catch (error) {
+        if(error.response){
+            console.log()
+        }
+    }
 }
 
 module.exports = {
