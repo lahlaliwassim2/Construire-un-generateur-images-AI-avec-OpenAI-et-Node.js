1251ddb7d2da1c4ee8acc8ca8a1e5806a5cb6d5f
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-27 10:12:13 +0100

    configuration d openai

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,3 +1,10 @@
+const { Configuration, OpenAIApi } = require("openai")
+
+const configuration = new Configuration({
+apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const generateImage = (req,res)=>{
     res.status(200).json({
         succes:true,
