309a4baa06ce67801ceb781229eb40559fce2290
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-27 09:48:55 +0100

    add Route & Controller

---
+++ controllers/openaiController.js
@@ -0,0 +1,10 @@
+const generateImage = (req,res)=>{
+    res.status(200).json({
+        succes:true,
+        
+    })
+}
+
+module.exports = {
+    generateImage
+}
--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use('/openai',require('./routes/openaiRoutes'))
+
 app.listen(port , ()=>{console.log(`server run in ${port}`)})
